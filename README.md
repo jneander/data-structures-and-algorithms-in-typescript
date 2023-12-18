484df7947b61f7039e892d59620f38b2eb7986f5
Jeremy Neander <jeremy.neander@gmail.com>
2023-12-18 07:45:45 -0600

    chore: configure eslint and prettier

--- README.md
+++ README.md
@@ -1,14 +1,24 @@
 # Data Structures and Algorithms in TypeScript
 
-Except where explicitly noted otherwise, the code in this repository is
-UNLICENSED. The purpose of this repo is to serve as a reference for concepts in
-computer science and their implementations in TypeScript. The value is
-knowledge, not a solution to be copy+pasted elsewhere. Developing this knowledge
-benefits from learning the concepts and then applying them in your own code, as
-has been done here.
+Except where explicitly noted otherwise, the code in this repository is UNLICENSED. The purpose of
+this repo is to serve as a reference for concepts in computer science and their implementations in
+TypeScript. The value is knowledge, not a solution to be copy+pasted elsewhere. Developing this
+knowledge benefits from learning the concepts and then applying them in your own code, as has been
+done here.
 
 ## Development
 
-This repo uses TypeScript for development feedback and somewhat improved
-documentation. To validate the usage of types in the source, run `test:types`.
-To validate continuously, run `test:types:watch`.
+This repo uses TypeScript for development feedback and somewhat improved documentation. To validate
+the usage of types in the source, run `test:types`. To validate continuously, run
+`test:types:watch`.
+
+### prettier
+
+To maximize consistency in code style, this repo uses prettier. To validate code style, run
+`lint:prettier`. To automatically fix any prettier issues, run `lint:prettier:fix`.
+
+### eslint
+
+To minimize issues from problematic practices and to provide additional feedback in configured IDEs,
+this repo uses eslint. To validate code against preconfigured eslint rules, run `lint:eslint`. To
+automatically fix any eslint issues, run `lint:eslint:fix`.
---
+++ config/eslint.config.cjs
@@ -0,0 +1,112 @@
+module.exports = {
+  env: {
+    es6: true,
+    node: true,
+  },
+
+  extends: [
+    'eslint:recommended',
+    'plugin:eslint-comments/recommended',
+    'plugin:promise/recommended',
+    'plugin:@typescript-eslint/eslint-recommended',
+    'plugin:@typescript-eslint/recommended',
+    'prettier',
+  ],
+
+  globals: {},
+  ignorePatterns: ['**/dist', '*.d.ts'],
+  parser: '@typescript-eslint/parser',
+
+  parserOptions: {
+    ecmaVersion: 2022,
+    sourceType: 'module',
+  },
+
+  plugins: ['import', 'prettier', 'promise', 'simple-import-sort', 'unused-imports'],
+  root: true,
+
+  rules: {
+    '@typescript-eslint/consistent-type-imports': 'error',
+    '@typescript-eslint/no-non-null-assertion': 'error',
+    '@typescript-eslint/no-unused-vars': ['error', {argsIgnorePattern: '^_'}],
+    '@typescript-eslint/no-var-requires': 'off',
+    'arrow-body-style': 'off',
+    curly: 'error',
+    'eslint-comments/disable-enable-pair': 'off',
+    'eslint-comments/no-unlimited-disable': 'off',
+    'eslint-comments/no-unused-disable': 'error',
+    'import/extensions': ['error', 'ignorePackages', {js: 'never', ts: 'never', tsx: 'never'}],
+    'import/no-extraneous-dependencies': ['error', {devDependencies: true}],
+
+    'lines-between-class-members': [
+      'error',
+      'always',
+      {
+        exceptAfterSingleLine: true,
+      },
+    ],
+
+    'no-cond-assign': ['error', 'always'],
+    'no-empty': ['error', {allowEmptyCatch: true}],
+    'no-fallthrough': 'off',
+    'no-sequences': ['error', {allowInParentheses: false}],
+    'no-unused-vars': 'off',
+    'object-shorthand': 'error',
+    'one-var': ['error', 'never'],
+
+    'padding-line-between-statements': [
+      'error',
+      {blankLine: 'always', prev: '*', next: 'continue'},
+      {blankLine: 'always', prev: '*', next: 'return'},
+      {blankLine: 'always', prev: '*', next: 'multiline-block-like'},
+      {blankLine: 'always', prev: 'multiline-block-like', next: '*'},
+      {blankLine: 'always', prev: 'multiline-expression', next: '*'},
+      {blankLine: 'always', prev: '*', next: 'multiline-expression'},
+      {blankLine: 'always', prev: 'multiline-const', next: '*'},
+      {blankLine: 'always', prev: 'multiline-let', next: '*'},
+      {blankLine: 'always', prev: 'multiline-var', next: '*'},
+      {blankLine: 'always', prev: '*', next: 'function'},
+      {blankLine: 'always', prev: 'function', next: '*'},
+      {blankLine: 'always', prev: ['case', 'default'], next: '*'},
+    ],
+
+    'prefer-arrow-callback': 'off',
+    'prettier/prettier': 'error',
+    'promise/always-return': 'off',
+
+    'simple-import-sort/imports': [
+      'warn',
+
+      {
+        groups: [
+          // Side effect imports.
+          ['^\\u0000'],
+
+          // Node.js builtins prefixed with `node:`.
+          ['^node:'],
+
+          // Packages.
+          // Things that start with a letter (or digit or underscore), or `@` followed by a letter.
+          ['^@?\\w'],
+
+          // Relative imports.
+          // Anything that starts with a dot.
+          ['^\\.'],
+        ],
+      },
+    ],
+
+    'simple-import-sort/exports': 'error',
+    'unused-imports/no-unused-imports': 'error',
+  },
+
+  settings: {
+    'import/parsers': {
+      '@typescript-eslint/parser': ['.ts'],
+    },
+
+    'import/resolver': {
+      typescript: {},
+    },
+  },
+}
